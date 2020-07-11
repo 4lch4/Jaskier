5f43ef6470a0ae83582bb97d0f45bc3696712116
Devin W. Leaman <dleaman212@gmail.com>
2020-07-11 17:18:38 -0500

    Deploy Next.js to gh-pages

---
+++ _next/static/74Myjtwmau_p5MMDEi6MU/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a],"/posts/[slug]":[a,"static\u002Fcss\u002F98db2618ce1f999557b8.css"]}}("static\u002Fchunks\u002F679ec97d13db2b8019a6ded39f4ddc9b409049a9.816b003ed7e233e2e10d.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
