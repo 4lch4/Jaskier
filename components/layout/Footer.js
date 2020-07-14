110ce127c7358e7447dfa2f28326c3b7b22d365c
Devin W. Leaman <dleaman212@gmail.com>
2020-07-14 16:26:54 -0500

    Updating Footer

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -1,4 +1,3 @@
-import { EXAMPLE_PATH } from '../../lib/constants'
 import Container from './Container'
 
 export default function Footer() {
@@ -7,20 +6,36 @@ export default function Footer() {
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
           <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Statically Generated with Next.js.
+            Jaskier
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
-              href="https://nextjs.org/docs/basic-features/pages"
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
+              className="contact-logo"
+              href="https://github.com/4lch4"
+              target="_blank"
             >
-              Read Documentation
+              <img alt="GitHub Logo" src="/assets/img/github.svg" />
             </a>
             <a
-              href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
-              className="mx-3 font-bold hover:underline"
+              className="contact-logo"
+              href="https://dev.to/4lch4"
+              target="_blank"
             >
-              View on GitHub
+              <img alt="Dev.to Logo" src="/assets/img/dev.to.svg" />
+            </a>
+            <a
+              className="contact-logo"
+              href="https://www.linkedin.com/in/devin-leaman-49622429/"
+              target="_blank"
+            >
+              <img alt="LinkedIn Logo" src="/assets/img/linkedin.svg" />
+            </a>
+            <a
+              className="contact-logo"
+              href="https://www.facebook.com/4lch4"
+              target="_blank"
+            >
+              <img alt="Facebook Logo" src="/assets/img/facebook.svg" />
             </a>
           </div>
         </div>
